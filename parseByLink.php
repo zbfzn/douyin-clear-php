7c1022de7dbc89a86e9e021db0f32938b3a3c7a7
zbfzn <41107747+zbfzn@users.noreply.github.com>
2019-07-11 12:56:00 +0800

    Update parseByLink.php
    接口获取流程优化，不能使用时自行更换douyinDevice.txt的信息，格式要一致

--- parseByLink.php
+++ parseByLink.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Created by zbfzn,My GitHub: https://github.com
+ * 依赖文件douyinDevice.txt
+ */
 header("Content-Type:text/html;charset=utf-8");
 error_reporting(0);
 $url=@$_GET['url'];//抖音视频地址
@@ -71,52 +75,80 @@ class Douyin{
         return $videoData;
 
     }
+    private function getDevices(){
+        return explode("\n",file_get_contents("./douyinDevice.txt"));
+    }
+    private function getApis($deviceInfos,$api_n,&$api_positions){
+        $base_api="https://aweme.snssdk.com/aweme/v1/aweme/detail/?origin_type=link&retry_type=no_retry&{}&ac=wifi&channel=update&aid=1128&app_name=aweme&version_code=251&version_name=2.5.1&device_platform=android&ssmix=a&device_type=MI+8&device_brand=xiaomi&language=zh&os_api=22&os_version=5.1.1&uuid=865166029463703&openudid=ec6d541a2f7350cd&manifest_version_code=251&resolution=1080*1920&dpi=480&update_version_code=2512&ts=1561136204&as=a1e500706c54fd8c8d&cp=004ad55fc8d60ac4e1&aweme_id=";
+        $apis=[];
+        $devices_size=sizeof($deviceInfos);
+        for($i=0;$i<($devices_size<$api_n?$devices_size:$api_n);$i++){
+            try{
+                $rand=random_int(0,sizeof($deviceInfos)-1);
+            }catch (Exception $e){
+                exit($this->getOutPutForError("PHP随机数错误"));
+            }
+            $apis[]=str_replace("{}",$deviceInfos[$rand],$base_api);
+            $api_positions[]=$rand;
+        }
+        return $apis;
+    }
+    /**
+     * @param $url
+     * @param $isFormat
+     * @param $old
+     * @return false|string
+     */
     private function parseVideoByLink($url,$isFormat,$old){
         $awemeId=$this->getAwemeId($url,$this->UA);
-        $api_position=-1;
-        $apis=[
-            "https://aweme.snssdk.com/aweme/v1/aweme/detail/?origin_type=link&retry_type=no_retry&iid=40848822342&device_id=56150513062&ac=wifi&channel=update&aid=1128&app_name=aweme&version_code=251&version_name=2.5.1&device_platform=android&ssmix=a&device_type=MI+8&device_brand=xiaomi&language=zh&os_api=22&os_version=5.1.1&uuid=865166029463703&openudid=ec6d541a2f7350cd&manifest_version_code=251&resolution=1080*1920&dpi=480&update_version_code=2512&ts=1561136204&as=a1e500706c54fd8c8d&cp=004ad55fc8d60ac4e1&aweme_id=",//更换设备信息
-            "https://aweme.snssdk.com/aweme/v1/aweme/detail/?origin_type=link&retry_type=no_retry&iid=74655440239&device_id=57318346369&ac=wifi&channel=wandoujia&aid=1128&app_name=aweme&version_code=140&version_name=1.4.0&device_platform=android&ssmix=a&device_type=MI+8&device_brand=xiaomi&os_api=22&os_version=5.1.1&uuid=865166029463703&openudid=ec6d541a2f7350cd&manifest_version_code=140&resolution=1080*1920&dpi=1080&update_version_code=1400&ts=1561136201&as=a13520b0e9c40d9cbd&cp=064fdf579fdd07cae1&aweme_id=",
-            "https://aweme.snssdk.com/aweme/v1/aweme/detail/?origin_type=link&retry_type=no_retry&iid=75364831157&device_id=68299559251&ac=wifi&channel=wandoujia&aid=1128&app_name=aweme&version_code=650&version_name=6.5.0&device_platform=android&ssmix=a&device_type=xiaomi+8&device_brand=xiaomi&language=zh&os_api=22&os_version=5.1.1&openudid=2e5c5ff4ce710faf&manifest_version_code=660&resolution=1080*1920&dpi=480&update_version_code=6602&mcc_mnc=46000&js_sdk_version=1.16.2.7&ts=1561136206&as=a1257080aec45ddcad&cp=0b4cd25fe4d00ccfe1&aweme_id="
-        ];
+        $api_positions=[];
+        $api_positions_error=[];
+        $api_n=4;//控制每次取得设备信息数量
+        $deviceInfos=$this->getDevices();
+        $apis=$this->getApis($deviceInfos,$api_n,$api_positions);
         $header=array("Accept-Encoding: utf-8",
             "User-Agent: okhttp/3.10.0.1"
         );
         $context=stream_context_create(array("http"=>array("header"=>$header)));
         if($awemeId){
             $isSuccess=false;
+            $count=-1;
             foreach ($apis as $api){
-                $ts=time();
-                $_rticket=$ts.'182';
-                $api_position++;
-                $data=json_decode(file_get_contents($api.$awemeId."&ts=$ts&_rticket=$_rticket",0,$context),true);
+                $count++;
+                $api_position=$api_positions[$count];
+                $data=json_decode(file_get_contents($api.$awemeId,0,$context),true);
                 $detail=@$data['aweme_detail'];
                 $short_id=@$data['aweme_detail']['author']['short_id'];
                 if($detail&&$short_id){
                     $isSuccess=true;
                     break;
                 }
+                $api_positions_error[]=$api_position;
             }
-
+            $str_position=null;//储存失败接口位置
+            foreach ($api_positions_error as $p){
+                $str_position=$str_position."[$p]";
+            }
             if(!$isSuccess){
-                return $this->getOutPutForError("抖音接口调用失败");
+                return $this->getOutPutForError("抖音接口调用失败$str_position");
             }
             if($old){
                 if($isFormat) {
                     $out = $this->getFormatVideoData($data['aweme_detail']);
                 }else{
-                    $out=$data['aweme_detail'];
+                    $out['data']=$data['aweme_detail'];
                 }
                 $out['status']=true;
                 $out['api_position']=$api_position;
-                $out['dataType_new']=!old;
+                $out['dataType_new']=!$old;
                 return json_encode($out);
             }
             $out=[
                 'status'=>true,
                 'data'=>null,
                 'api_position'=>$api_position,
-                'dataType_new'=>!$old
+                'dataType_new'=>!$old,
+                'error_api'=>$str_position
             ];
             if($isFormat) {
                 $out['data'] = $this->getFormatVideoData($data['aweme_detail']);
